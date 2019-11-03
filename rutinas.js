f0b574d15ac6c7550e632e8fd3e692b74b03c0fc
Victor Julio Gomez <41869547+vgomezc81972@users.noreply.github.com>
2019-11-03 11:21:32 -0500

    Guia2 - BSC
    Programa de BSC - Indcadores

--- rutinas.js
+++ rutinas.js
@@ -59,7 +59,10 @@ function validarTecla(event,li_tipo){
                   patron =/[0123456789/]/; 
 			break;
 		case "FE": // Unicamente permite numeros y el -
-                  patron =/[0123456789/-]/; 
+                  patron =/[0123456789-]/; 
+			break;
+		case "EM": // Unicamente permite numeros y el -
+                  patron =/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; 
 			break;
 		case "EN": // Unicamente permite numeros
                   patron = /\d/;  
@@ -83,23 +86,6 @@ function validarTecla(event,li_tipo){
 }
 
 
-function Mostrar(){
- try{   
-   for (var i=0;i<document.forms.length;i++){
-     len     =       document.forms[i].elements.length;
-     var     index   =       0;
-     for( index=0; index < len; index++ ){
-       if (document.forms[i].elements[index].type=="submit" || document.forms[i].elements[index].type=="button"){
-          document.forms[i].elements[index].style.display='';
-      }
-     }    
-   }
-  }
-  catch(e){
-  }
-  return true;
-}
-
 function validarEmail(email) 
 {
      if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)){
@@ -108,108 +94,98 @@ function validarEmail(email)
      return (false);
 }  
 
-function jesfecha(fstring)
-{
-	var ano;
-	var mes;
-	var dia;
-	if(FormatoFecha == "AAAA-MM-DD"){
-		ano = parseInt(fstring.substring(0,4),10);
-		mes = parseInt(fstring.substring(5,7),10);
-		dia = parseInt(fstring.substring(8,10),10);
+function alerta()
+    {
+    var mensaje;
+    var opcion = confirm("Digite en Aceptar o Cancelar");
+    if (opcion == true) {
+        mensaje = "Has Digitado OK";
+	} else {
+	    mensaje = "Has Digitado Cancelar";
 	}
-	else if(FormatoFecha == "DD/MM/AAAA"){
-		dia = parseInt(fstring.substring(0,2),10);
-		mes = parseInt(fstring.substring(3,5),10);
-		ano = parseInt(fstring.substring(6,10),10);
-	}
-	var vanoini = AnoInicial;
-	var vanofin = AnoFinal;
-
-       /*
-	if(isNaN(parseInt(anoini, 10)) == false)
-		vanoini = parseInt(anoini, 10);
-
-	if(isNaN(parseInt(anofin, 10)) == false)
-		vanofin = parseInt(anofin, 10);
-		
-      */
-
-	if(fstring.length != 10)
-	{
-		mostrar_error(7, fstring, FormatoFecha);
-		return false;
-	}
-	if(isNaN(dia) == true)
-	{
-		mostrar_error(8, fstring.substring(8, 10));
-		return false;
-	}
-	if(isNaN(mes) == true)
-	{
-		mostrar_error(9, fstring.substring(5, 7));
-		return false;
-	}
-	if(isNaN(ano) == true)
-	{
-		mostrar_error(10, fstring.substring(0, 4));
-		return false;
-	}
-	if(ano < vanoini || ano > vanofin)
-	{
-		mostrar_error(11, ano, vanoini, vanofin);
-		return false;
-	}
-	if(mes < 1 || mes > 12)
-	{
-		mostrar_error(12, mes);
-		return false;
-	}
-	if(dia < 1 || dia > 31)
-	{
-		mostrar_error(13, dia);
-		return false;
-	}
-	var bisiesto = (Math.floor(ano/4) * 4 ) - ano;
-	/* si da cero es bisiesto */
-	if(( (mes==2 || mes==4 || mes==6 || mes==9 || mes==11) && dia==31)
-	   || (mes==2 && bisiesto==0 && dia>29) || (mes==2 && bisiesto!=0 && dia>28))
-	{
-		mostrar_error(14, dia);
-		return false;
-	}
-	return true;
+	document.getElementById("ejemplo").innerHTML = mensaje;
 }
 
-function mostrar_error(error)
-{
-	var args = mostrar_error.arguments;
-	var Errores = new Array();
-	Errores[0] = "Sesion no valida.";
-	Errores[1] = "El campo " + args[1] + " es obligatorio.";
-	Errores[2] = "El campo " + args[1] + "( " + args[2] + " ) debe ser numérico.";
-	Errores[3] = "El campo " + args[1] + "( " + args[2] + " ) debe ser mayor o igual a " + args[3];
-	Errores[4] = "El campo " + args[1] + "( " + args[2] + " ) debe ser menor o igual a " + args[3];
-	Errores[5] = "El campo " + args[1] + " no debe tener decimales ";
-	Errores[6] = "El campo " + args[1] + " debe ser una fecha válida (" + args[2] + ")";
-	Errores[7] = "La fecha " + args[1] + " debe estar en formato " + args[2];
-	Errores[8] = "El día " + args[1] + " no es un número.";
-	Errores[9] = "El mes " + args[1] + " no es un número.";
-	Errores[10] = "El año " + args[1] + " no es un número.";
-	Errores[11] = "El año " + args[1] + " debe estar entre " + args[2] + " y " + args[3] + ".";
-	Errores[12] = "El mes " + args[1] + " es inválido.";
-	Errores[13] = "El día " + args[1] + " es inválido.";
-	Errores[14] = "El día " + args[1] + " es inválido para el mes digitado.";
-	Errores[15] = "El campo " + args[1] + " no puede tener mas de " + args[2] + " caracteres.";
-	Errores[16] = "El campo " + args[1] + "( " + args[2] + " ) debe ser un email válido.";
-	Errores[17] = "";
-	Errores[18] = "";
-	Errores[19] = "";
-	Errores[20] = "";
-	Errores[21] = "El campo " + args[1] + " debe tener mínimo " + args[2] + " caracter(es).";
-	Errores[22] = "El campo " + args[1] + " debe ser igual al campo " + args[2] + ".";
-	Errores[23] = "El usuario se encuentra INACTVO. Por favor comunicarse con el CALL CENTER";
-	Errores[24] = "El campo " + args[1] + " debe tener máximo " + args[2] + " caracter(es).";
+function showBusiness() {
+    var title = ["Apps Móviles", "Diseño de Sitios", "UI/UX"];
+    var classIcon = ["glyphicon-phone", "glyphicon-globe", "glyphicon-eye-open"];
+    var description = ["Diseñamos y desarrollamos aplicaciones móviles y usables para todos los sectores.", "Construimos página web a la medida sinedo usuables, interactivas y rápidas.", "Realizamos consultorías para usabilidad de sitios web y aplicaciones."];
+    var i;
+    var text = "";
+    for (i = 0; i < title.length; i += 1) {
+        text += "<div class='itemBusiness'><span class='glyphicon " + classIcon[i] + "' aria-hidden='true'></span><h3>" + title[i] + "</h3><p>" + description[i] + "</p></div>";
+    }
+    document.getElementById("sectionBusiness").innerHTML = text;
+}
 
-	alert(Errores[error]);
+function showMenu() {
+    var values = ["Inicio", "Indicadores", "Registro Movimiento", "Registro Analisis y Estrategias", "Hoja de Vida del Indicador"];
+    var links = ["./general.html", "./Indicadores.html", "./movimiento.html", "./analisis.html", "./HojaVidaP.html"];
+    var i = 0;
+    var text = "";
+    while (values[i]) {
+        text += "<a class='itemMenu' href='" + links[i] + "'>" + values[i] + "</a>";
+        i += 1;
+    }
+    document.getElementById("showMenu").innerHTML = "<div id='overlayMenu' class='animated bounce'></div>";
+    document.getElementById("overlayMenu").innerHTML = text;
+}
+
+
+function agregar_movimiento(){
+  cont++;
+  var _oid = document.getElementById("oid").value;
+  var _cod = document.getElementById("codigoIndicador").value;
+  var _num = document.getElementById("numerador").value;
+  var _den = document.getElementById("denominador").value;
+  
+  if(_num.trim()==''){
+     alert("Ingrese numerador");
+  }
+    
+  var fila='<tr class="selected" id="fila'+cont+'" onclick="seleccionar(this.id);"><td>'+cont+'</td><td>'+_oid+'</td><td>'+_cod+'</td><td>'+_num+'</td><td>'+_den+'</td></tr>';
+  $('#tabla').append(fila);
+  reordenar();
+
+}
+
+function agregar_analisis(){
+  cont++;
+  var _oid = document.getElementById("oid").value;
+  var _cod = document.getElementById("codigoIndicador").value;
+  var _ana = document.getElementById("analisis").value;
+  var _est = document.getElementById("estrategias").value;
+  
+  if(_ana.trim()==''){
+     alert("Ingrese analisis");
+  }
+    
+  var fila='<tr class="selected" id="fila'+cont+'" onclick="seleccionar(this.id);"><td>'+cont+'</td><td>'+_oid+'</td><td>'+_cod+'</td><td>'+_ana+'</td><td>'+_est+'</td></tr>';
+  $('#tabla').append(fila);
+  reordenar();
+
+}
+	
+function seleccionar(id_fila){
+	if($('#'+id_fila).hasClass('seleccionada')){
+		$('#'+id_fila).removeClass('seleccionada');
+	}
+	else{
+		$('#'+id_fila).addClass('seleccionada');
+	}
+	id_fila_selected=id_fila
+}
+
+function eliminar(id_fila){
+   $('#'+id_fila).remove();	
+   reordenar();
+}
+
+
+function reordenar(){
+	var num=1;
+	$('#TableBody tbody tr').each(function(){
+	   $(this).find('td').eq(0).text(num);
+	num++;
+	});
 }
